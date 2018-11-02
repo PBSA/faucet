d1b713ff31286113cbbe071f66ba0eba5cc1f97b
Fabian Schuh <mail@xeroc.org>
2018-11-02 14:47:12 +0100

    worker for automated donations after account registration

---
+++ worker_donations.py
@@ -0,0 +1,47 @@
+import yaml
+from pprint import pprint
+import sys
+import json
+from peerplays import PeerPlays
+from peerplays.account import Account
+from peerplays.blockchain import Blockchain
+
+config = yaml.load(open("config.yml").read())
+
+instance = PeerPlays(
+    config["witness_url"],
+    keys=[config["wif"]],
+    nobroadcast=config["nobroadcast"]
+)
+
+def run(begin=None, end=None):
+
+    blockchain = Blockchain(
+        mode="head",
+        blockchain_instance=instance
+    )
+
+    for op in blockchain.stream(
+        opNames=["account_create"],
+        start=int(begin) if begin else None,
+        stop=int(end) if end else None,
+    ):
+        blockid = op.get("block_num")
+        timestamp = op.get("timestamp")
+
+        #if not blockid % 100:
+        #    print("Blockid: %d (%s)" % (blockid, timestamp), flush=True)
+
+        try:
+            pprint(instance.transfer(
+                op["name"],
+                config["donation_amount"], config["donation_asset"],
+                account=config["registrar"]
+            ))
+        except Exception as e:
+            print(str(e))
+            pass
+
+
+if __name__ == '__main__':
+    run()
